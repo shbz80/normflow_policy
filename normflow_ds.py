fix nan with a svd decomposition to sample and eval logprob in the subspace. Also fix the stupid mistake of entropy eval. Now both regular DiagGaussian and LinearSubspace train. The problem of latter is that SVD is super slow, 10x-20x? have no idea if avoid perturbations along the vel direction are sufficient for policies of good optimality. Idea of truncated Gaussian along vel direction when it is non-zero might help. But have not got a very elegant theoretical understanding of transition between zero/non-zero phases, and the potential numerical stability as well...

normflow_ds.py
@@ -159,7 +159,7 @@ class NormalizingFlowDynamicalSystem(nn.Module):
         return (batch_size, x_dot_dim, x_dot_dim)
         '''
         #note we can avoid matrix inversion because x_dot are vectors so we actually just need the inverse of norm
-        norm_square_inv = 1./torch.sum(x_dot**2, dim=1, keepdim=True).clamp(min=1e-4)
+        norm_square_inv = 1./torch.sum(x_dot**2, dim=1, keepdim=True).clamp(min=1e-6)
         # print('x_dot', x_dot)
         I = torch.eye(x_dot.shape[1], device=self.device).unsqueeze(0).repeat(x_dot.shape[0], 1, 1)
         return I - norm_square_inv.unsqueeze(-1)*torch.bmm(x_dot.unsqueeze(-1), x_dot.unsqueeze(1))
@@ -218,6 +218,7 @@ class NormalizingFlowDynamicalSystemActorProb(nn.Module):
         return (mu, sigma), None
 
 from normflow_policy.utils import LinearSubSpaceDiagGaussian
+from tianshou.policy.dist import DiagGaussian
 
 class NormalizingFlowDynamicalSystemPPO(PPOPolicy):
     def __init__(self,
@@ -258,19 +259,15 @@ class NormalizingFlowDynamicalSystemPPO(PPOPolicy):
             more detailed explanation.
         """
         (mu, sigma), h = self.actor(batch.obs, state=state, info=batch.info)
-        
+        U = None
+
+        #comment these out to use regular diagonal gaussian exploration
         batch_x_dot = to_torch(batch.obs[:, self.actor.normflow_ds.dim:], device=self.actor.device, dtype=torch.float32)
-
         nullspace_mat = self.actor.normflow_ds.null_space(batch_x_dot)
-
-        #construct conv factor, also add a small regularization term to keep it valid
-        # cov = torch.bmm(torch.bmm(nullspace_mat, torch.diag_embed(sigma)), nullspace_mat.transpose(1, 2)) + torch.diag_embed(torch.ones_like(sigma))*1e-4
+        U, _, _ = torch.pca_lowrank(nullspace_mat, q=self.actor.normflow_ds.dim-1)
         
-        # cov = torch.diag_embed(sigma)
-        #must be multivariate gaussian        
-        dist = self.dist_fn(loc=mu, scale=sigma, lintrans=nullspace_mat)
+        dist = self.dist_fn(loc=mu, scale=sigma, lintrans=U)
 
-        # act = mu + torch.bmm(nullspace_mat, (torch.randn_like(mu)*sigma).unsqueeze(-1)).squeeze(-1) 
         act = dist.sample()
 
         if self._range:

utils.py
@@ -18,20 +18,52 @@ def _batch_mv(bmat, bvec):
 
 class LinearSubSpaceDiagGaussian(DiagGaussian):
     """Diagonal Gaussian distribution in specified linear subspace"""
+    """This is used as a regular diag-gaussian if transformation is not given"""
     def __init__(self, loc, scale, lintrans, validate_args=None):
-        super().__init__(torch.zeros_like(loc), scale, validate_args)
+        if lintrans is not None:
+            scale = scale[:, :-1]
+        super().__init__(torch.zeros_like(scale), scale, validate_args)
+        
+        self.loc_real = loc
         self.lintrans = lintrans
-        self.lintrans_pinv = torch.pinverse(self.lintrans)
-    
+        if lintrans is not None:
+            #self.lintrans_pinv = torch.pinverse(self.lintrans)
+            self.lintrans_pinv = lintrans.transpose(-2, -1)
+        else:
+            self.lintrans_pinv = None
+
     def sample(self, sample_shape=torch.Size()):
         samples = super().sample(sample_shape)
-        #apply linear transform to samples
-        return _batch_mv(self.lintrans, samples) + self.loc
+        if self.lintrans is not None:
+            #apply linear transform to samples
+            # print(self.lintrans.shape, samples.shape)
+            return _batch_mv(self.lintrans, samples) + self.loc_real
+        else:
+            #linear subspace is void, we are free to generate samples
+            return samples + self.loc_real
 
     def log_prob(self, actions):
         #convert them back to subspace and eval log prob
-        actions_sub = _batch_mv(self.lintrans_pinv, actions - self.loc)
+        if self.lintrans is not None:
+            #convert them back to subspace and eval log prob
+            actions_sub = _batch_mv(self.lintrans_pinv, actions - self.loc_real)
+        else:
+            actions_sub = actions - self.loc_real
         return super().log_prob(actions_sub).sum(-1, keepdim=True)
 
     def entropy(self):
-        return super().entropy().sum(-1)
+        return super().entropy()
+    
+
+if __name__ == '__main__':
+    batch_size=4
+    loc = torch.randn((batch_size, 5))
+    scale = torch.rand((batch_size, 5)) + 1
+    diag_dist = DiagGaussian(loc=loc, scale=scale)
+    linsub_dist = LinearSubSpaceDiagGaussian(loc=loc, scale=scale, lintrans=None)
+
+    ## are they identical?
+    samples = diag_dist.sample()
+    print(diag_dist.log_prob(samples), linsub_dist.log_prob(samples))
+    samples = linsub_dist.sample()
+    print(diag_dist.log_prob(samples), linsub_dist.log_prob(samples))